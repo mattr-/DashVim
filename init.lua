1e9a42a5d4eccdf72055920628b9ff8a05b96a13
Matt Rogers <codemattr@gmail.com>
2023-07-03 16:44:01 -0500

    Initial project skeleton
    Use APL 2.0 for the license to mirror LazyVim as if/when I take
    code/inspiration from there, I'm happy to use the same license.
    
    Add a short README talking about what I'm doing here.
    
    Set up folke/neoconf.nvim for this project and a stylua config for
    formatting.
    
    Last, but not least, add lazy.nvim as a core plugin.

---
+++ init.lua
@@ -0,0 +1,10 @@
+vim.api.nvim_echo({
+	{
+		"Do not use this repository directly\n",
+		"ErrorMsg",
+	},
+	{ "Press any key to exit", "MoreMsg" },
+}, true, {})
+
+vim.fn.getchar()
+vim.cmd([[quit]])
