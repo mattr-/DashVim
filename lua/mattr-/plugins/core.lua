19af3e1ee12521d60441a289c2da11cd984fd07e
Matt Rogers <codemattr@gmail.com>
2023-07-04 16:00:25 -0500

    Run config initialization in `plugins/core`
    For future reference, this works because we've set our own plugin to be
    priority 10000 and we don't lazily load it. These two things combine to
    make all the rest work.

--- lua/mattr-/plugins/core.lua
+++ lua/mattr-/plugins/core.lua
@@ -1,3 +1,6 @@
+-- Do basic config setup before loading plugins
+require ("mattr-.config").init()
+
 return {
   { "folke/lazy.nvim", version = "*" },
   { "mattr-/mattr-.nvim", priority = 10000, lazy = false, config = true, cond = true, version = "*" },
