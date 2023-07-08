1879e16934f452f94e59f3b77bad3dd11eaaea98
Matt Rogers <codemattr@gmail.com>
2023-07-07 21:25:09 -0500

    Add a module for getting color information
    Currently used only for the foreground color

---
+++ lua/mattr-/colors/init.lua
@@ -0,0 +1,9 @@
+local M = {}
+
+function M.fg(name)
+  local hl = vim.api.nvim_get_hl(0, { name = name })
+  local fg = hl and hl.fg or hl.foreground
+  return fg and { fg = string.format("#%06x", fg) }
+end
+
+return M
