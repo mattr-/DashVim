641617d2424a73e4acbfa8866a4fec01b0de9c3f
Matt Rogers <codemattr@gmail.com>
2023-07-16 09:55:59 -0500

    Allow autocmds and keymaps to be extended locally
    This lets me try things out before moving it to the main framework

--- lua/mattr-/config/init.lua
+++ lua/mattr-/config/init.lua
@@ -38,14 +38,14 @@ function M.setup(opts)
       group = vim.api.nvim_create_augroup("LazyVim", { clear = true }),
       pattern = "VeryLazy",
       callback = function()
-        -- M.load("autocmds")
-        -- M.load("keymaps")
+        M.load("autocmds")
+        M.load("keymaps")
       end,
     })
   else
     -- load them now so they affect the opened buffers
-    --M.load("autocmds")
-    --M.load("keymaps")
+    M.load("autocmds")
+    M.load("keymaps")
   end
 
   require("lazy.core.util").try(function()
