0c31b57c8b3c5043a8728dc6f76fd9bd29b0a38f
Matt Rogers <codemattr@gmail.com>
2023-07-16 09:58:26 -0500

    Configure notify differently if noice is installed

--- lua/mattr-/plugins/ui.lua
+++ lua/mattr-/plugins/ui.lua
@@ -12,6 +12,18 @@ return {
         return math.floor(vim.o.columns * 0.75)
       end,
     },
+    init = function()
+      -- when noice is not enabled, install notify on VeryLazy
+      local Plugin = require("mattr-.config.plugins")
+      if not Plugin.has("noice.nvim") then
+        vim.api.nvim_create_autocmd("User", {
+          pattern = "VeryLazy",
+          callback = function()
+            vim.notify = require("notify")
+          end,
+        })
+      end
+    end,
   },
 
   -- better vim.ui
