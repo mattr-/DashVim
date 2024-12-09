b572adcd56c9e9f56d8d132858a647e9d6681c9d
Matt Rogers <codemattr@gmail.com>
2024-12-09 13:30:52 -0600

    chore(init): require neovim 0.10 or later

--- lua/dashvim/plugins/init.lua
+++ lua/dashvim/plugins/init.lua
@@ -1,3 +1,14 @@
+-- This file is the very first one loaded from the framework by lazy.nvim
+-- so we do a bit of setup in here.
+if vim.fn.has("nvim-0.10.0") == 0 then
+  vim.api.nvim_echo({
+    { "DashVim requires neovim >= 0.10.0\n", "ErrorMsg" },
+    { "\nPress any key to exit...", "MoreMsg" },
+  }, true, {})
+  vim.fn.getchar()
+  vim.cmd([[quit]])
+end
+
 return {
   { "folke/lazy.nvim", version = "*" },
   { "mattr-/DashVim", priority = 10000, lazy = false, opts = {}, cond = true, version = "*" },
