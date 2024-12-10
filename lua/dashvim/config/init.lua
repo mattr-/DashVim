3ffaf6590aebd302a7e082a3a838b5b72764200b
Matt Rogers <codemattr@gmail.com>
2024-12-09 18:44:44 -0600

    chore: cleanup the config
    Several cleanups that I didn't feel like breaking out into multiple
    commits.
     - Use a function for the colorscheme so it's lazy loaded
     - rename the `defaults` option to `builtins`. If you don't want
       DashVim's autocmds or keymaps, you can use this for the plugin spec:
       ```
       {
          "mattr-/DashVim",
          opts = {
            builtins = {
              autocmds = false,
              keymaps = false,
            },
          },
       }
       ```
     - Remove all the icons since I have nothing coded that will use them.
       They're left over from my personal config
     - Fix a wrong variable name if we can't load autocmds, options, or
       keymaps.
     - Use vim.notify for error display when we fail to load a colorscheme.

--- lua/dashvim/config/init.lua
+++ lua/dashvim/config/init.lua
@@ -3,69 +3,15 @@ local M = {}
 
 --- @class DashVimOptions
 local defaults = {
-  colorscheme = "tokyonight",
-  defaults = {
+  -- either a string with the colorscheme name or a function that loads the colorscheme
+  colorscheme = function()
+    require("tokyonight").load()
+  end,
+  builtins = {
     autocmds = true, -- dashvim.config.autocmds
     keymaps = true, -- dashvim.config.keymaps
     -- options cannot be configured here. they load before setup
     -- use `package.loaded["dashvim.config.options"] = true` to the top of your init.lua
-  },
-  icons = {
-    dap = {
-      Stopped = { "󰁕 ", "DiagnosticWarn", "DapStoppedLine" },
-      Breakpoint = " ",
-      BreakpointCondition = " ",
-      BreakpointRejected = { " ", "DiagnosticError" },
-      LogPoint = ".>",
-    },
-    diagnostics = {
-      Error = " ",
-      Warn = " ",
-      Hint = " ",
-      Info = " ",
-    },
-    git = {
-      added = " ",
-      modified = " ",
-      removed = " ",
-    },
-    kinds = {
-      Array = " ",
-      Boolean = " ",
-      Class = " ",
-      Color = " ",
-      Constant = " ",
-      Constructor = " ",
-      Copilot = " ",
-      Enum = " ",
-      EnumMember = " ",
-      Event = " ",
-      Field = " ",
-      File = " ",
-      Folder = " ",
-      Function = " ",
-      Interface = " ",
-      Key = " ",
-      Keyword = " ",
-      Method = " ",
-      Module = " ",
-      Namespace = " ",
-      Null = " ",
-      Number = " ",
-      Object = " ",
-      Operator = " ",
-      Package = " ",
-      Property = " ",
-      Reference = " ",
-      Snippet = " ",
-      String = " ",
-      Struct = " ",
-      Text = " ",
-      TypeParameter = " ",
-      Unit = " ",
-      Value = " ",
-      Variable = " ",
-    },
   },
 }
 
@@ -88,7 +34,7 @@ function M.load(name)
       require(mod)
     end, {
         msg = "Failed loading " .. mod,
-        on_error = function(msg)
+        on_error = function(message)
           local info = require("lazy.core.cache").find(mod)
           if info == nil or (type(info) == "table" and #info == 0) then
             return
@@ -99,7 +45,7 @@ function M.load(name)
       })
   end
   -- always load ours, then user file
-  if M.defaults[name] or name == "options" then
+  if M.builtins[name] or name == "options" then
     _load("dashvim.config." .. name)
   end
   _load("config." .. name)
@@ -143,8 +89,8 @@ function M.setup(opts)
   end, {
     msg = "Could not load your colorscheme",
     on_error = function(msg)
-      DashVim.error(msg)
-      vim.cmd.colorscheme("habamax")
+      vim.notify(msg, vim.log.levels.ERROR, {})
+      vim.cmd.colorscheme("vim")
     end,
   })
   DashVim.track()
