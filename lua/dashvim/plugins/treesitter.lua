7046a716ec9468824c50ea7ca96301311e08085c
Matt Rogers <codemattr@gmail.com>
2025-01-13 10:53:31 -0600

    feat(treesitter): add more laziness to how we load
    First up, we always use `main` for Treesitter. Second, we use the new
    `LazyFile` event to load when operating on a new buffer. Third, we add a
    bunch of commands to trigger loading.
    
    Last and certainly not least, we ensure that the `vim` and `vimdoc`
    parsers are installed. Other parsers need to be added as part of their
    language specifications.

--- lua/dashvim/plugins/treesitter.lua
+++ lua/dashvim/plugins/treesitter.lua
@@ -1,6 +1,26 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter",
+    version = false, -- use main for now, there's a rewrite happening in another branch
+    build = ":TSUpdate",
+    event = "LazyFile",
+    lazy = vim.fn.argc(-1) == 0, -- load early if we have arguments
+    -- treesitter has a lot of commands but we only load on the ones that don't
+    -- involve buffers since we load treesitter as part of LazyFile
+    cmd = {
+      "TSInstall",
+      "TSInstallSync",
+      "TSInstallInfo",
+      "TSUpdate",
+      "TSUpdateSync",
+      "TSUninstall",
+    },
     opts_extend = { "ensure_installed" },
+    opts = {
+      ensure_installed = {
+        "vim",
+        "vimdoc"
+      },
+    }
   },
 }
