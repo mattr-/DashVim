9dfdac3641e03f3f0e6e0d08af99fe126e247da2
Matt Rogers <codemattr@gmail.com>
2025-02-02 16:07:29 -0600

    feat(ui): use mini.icons for devicons
    with a comment for mocking out nvim-web-devicons because we'll need that
    but nothing is using icons yet.

--- lua/dashvim/plugins/ui.lua
+++ lua/dashvim/plugins/ui.lua
@@ -6,4 +6,14 @@ return {
     name = "catppuccin",
     opts = {}
   },
+
+  --mini.icons for filetype and other icons
+  {
+    "echasnovski/mini.icons",
+    lazy = true,
+    version = false, --pull from main always
+    opts = {},
+    -- hook into the package loader to have mini.icons mock out
+    -- nvim-web-devicons if something attempts to load nvim-web-devicons
+  },
 }
