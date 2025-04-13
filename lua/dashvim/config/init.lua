cd1191edfd85e6cc2e4dffbd58c210085b08bfe0
Matt Rogers <codemattr@gmail.com>
2025-04-12 21:23:24 -0500

    feat: load framework defaults then user settings
    This works for autocmds, options, and keymaps

--- lua/dashvim/config/init.lua
+++ lua/dashvim/config/init.lua
@@ -21,15 +21,29 @@ function M.setup(opts)
   options = vim.tbl_deep_extend("force", defaults, opts or {}) or {}
 end
 
+--- @param name "autocmds" | "keymaps" | "options"
+function M.cascade_load(name)
+  function _try_load(mod)
+    if require("lazy.core.cache").find(mod)[1] then
+      require("lazy.core.util").try(function()
+        require(mod)
+      end, { msg = "Failed loading " .. mod })
+    end
+  end
+
+  _try_load("dashvim.config." .. name)
+  _try_load("config.".. name)
+end
+
 M.early_init_done = false
 function M.early_init()
   if M.early_init_done then
     return
   end
 
-  require("dashvim.config.options")
-  require("dashvim.config.autocmds")
-  require("dashvim.config.keymaps")
+  M.cascade_load("options")
+  M.cascade_load("autocmds")
+  M.cascade_load("keymaps")
 end
 
 setmetatable(M, {
