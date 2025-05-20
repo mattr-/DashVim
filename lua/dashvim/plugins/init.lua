f310065aa862447ecd99de89c1a21e9b254e43a4
Matt Rogers <codemattr@gmail.com>
2025-05-20 13:47:12 -0500

    chore: give dashvim no opts
    opts might be empty by default but i want to be explicit about it

--- lua/dashvim/plugins/init.lua
+++ lua/dashvim/plugins/init.lua
@@ -3,6 +3,6 @@ require("dashvim.config").early_init()
 return
 {
   { "folke/lazy.nvim", version = "*" },
-  { "mattr-/DashVim", priority = 10000, lazy = false, cond = true, version = "*" },
+  { "mattr-/DashVim", priority = 10000, lazy = false, opts = {}, cond = true, version = "*" },
   { "folke/snacks.nvim", priority = 1000, lazy = false },
 }
